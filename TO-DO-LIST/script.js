064fb8b5d6db889654a164d37e07dfb0b4a7bd86
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2023-11-29 11:33:51 +0530

    Creating TO-DO-APP
    Creating TO-DO-APP using HTML , CSS , JAVASCRIPT

---
+++ TO-DO-LIST/script.js
@@ -0,0 +1,28 @@
+const item = document.querySelector("#item");
+const tobox = document.querySelector("#todobox");
+
+item.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    addtodo(this.value);
+    this.value = " ";
+  }
+});
+
+const addtodo = (item) => {
+  const listitem = document.createElement("li");
+  listitem.innerHTML = `
+  ${item}
+
+  <i class="fa-solid fa-xmark"></i>
+  
+  `;
+  listitem.addEventListener("click", function () {
+    this.classList.toggle("done");
+  });
+
+  listitem.querySelector("i").addEventListener("click", function () {
+    listitem.remove();
+  });
+
+  tobox.appendChild(listitem);
+};
